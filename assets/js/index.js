fe46ed0f9cd9678b8e732be43c00db52666b88c6
jarrodyellets <jarrod524@gmail.com>
2018-08-07 00:30:00 -0400

    Added animated hash position

--- assets/js/index.js
+++ assets/js/index.js
@@ -67,7 +67,7 @@ $(document).ready(function(){
   });
 
   $(window).scroll(function(){
-    $('.about').each(function(i){
+    $('.tools').each(function(i){
       var bottom_of_object = $(this).offset().top + $(this).outerHeight();
       var bottom_of_window = $(window).scrollTop() + $(window).height();
          if( bottom_of_window > bottom_of_object ){
