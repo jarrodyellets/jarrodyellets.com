2ad527d291ce9fdd205d763d559596799fc5836f
jarrodyellets <jarrod524@gmail.com>
2018-08-21 08:51:49 +0200

    Added dynamic underline who

--- assets/js/index.js
+++ assets/js/index.js
@@ -67,6 +67,14 @@ $(document).ready(function(){
   });
 
   $(window).scroll(function(){
+    $('.aboutWrapper').each(function(i){
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height(); 
+        if(bottom_of_window > bottom_of_object){
+          $('.underlineWho').addClass('right');
+        }
+    });
+
     $('.tools').each(function(i){
       var bottom_of_object = $(this).offset().top + $(this).outerHeight();
       var bottom_of_window = $(window).scrollTop() + $(window).height();
