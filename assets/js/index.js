64994e87f4aebe2bb224233d116101369cfb7f8c
jarrodyellets <jarrod524@gmail.com>
2018-10-15 12:00:01 +0200

    Redesign about tools

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,61 +1,63 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   var zIndex = 1;
   var topPic = "";
 
   // Nav Bar auto scroll to section
-  $('#about').click(function(){
-      scroll_to(".about");
-    });
-  $('#portfolio').click(function(){
-      scroll_to(".portfolio");
-    });
-  $('#contact').click(function(){
-      scroll_to(".contact")
-    });
+  $('#about').click(function () {
+    scroll_to(".about");
+  });
+  $('#portfolio').click(function () {
+    scroll_to(".portfolio");
+  });
+  $('#contact').click(function () {
+    scroll_to(".contact")
+  });
 
   // Arrow scroll to top
-  $('.arrow').click(function(){
-    $('html, body').animate({scrollTop: '0px'}, 500);
+  $('.arrow').click(function () {
+    $('html, body').animate({
+      scrollTop: '0px'
+    }, 500);
   })
 
   // Hamburger nav btns
-  $(".hamburger").click(function(){
+  $(".hamburger").click(function () {
     $(".navLinks").addClass("width");
   });
-  $(".closeBtn").click(function(){
+  $(".closeBtn").click(function () {
     $(".navLinks").removeClass("width");
   });
 
   // Nav bar polaroids
-  $("#about").mouseenter(function(){
-    if(topPic != "about"){
+  $("#about").mouseenter(function () {
+    if (topPic != "about") {
       $("#rolloverAbout").removeClass("rolloverShow");
       $("#rolloverAbout").css('z-index', zIndex);
       zIndex++
-      setTimeout(function(){
+      setTimeout(function () {
         topPic = "about";
         $("#rolloverAbout").addClass("rolloverShow");
       }, 10)
     }
   });
-  $("#portfolio").mouseenter(function(){
-    if(topPic != "portfolio"){
+  $("#portfolio").mouseenter(function () {
+    if (topPic != "portfolio") {
       $("#rolloverPortfolio").removeClass("rolloverShow");
       $("#rolloverPortfolio").css('z-index', zIndex);
       zIndex++
-      setTimeout(function(){
+      setTimeout(function () {
         topPic = "portfolio";
         $("#rolloverPortfolio").addClass("rolloverShow");
       }, 10)
     }
   });
-  $("#contact").mouseenter(function(){
-    if(topPic != "contact"){
+  $("#contact").mouseenter(function () {
+    if (topPic != "contact") {
       $("#rolloverContact").removeClass("rolloverShow");
       $("#rolloverContact").css('z-index', zIndex);
       zIndex++
-      setTimeout(function(){
+      setTimeout(function () {
         topPic = "contact";
         $("#rolloverContact").addClass("rolloverShow");
       }, 10)
@@ -63,40 +65,43 @@ $(document).ready(function(){
   });
 
   // About auto underline
-  $(window).scroll(function(){
-    $('.aboutWrapper').each(function(i){
-      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-      var bottom_of_window = $(window).scrollTop() + $(window).height(); 
-        if(bottom_of_window > bottom_of_object){
-          $('.underlineWho').addClass('right');
-          setTimeout(function(){
-            $('.underlineWhat').addClass('right');
-          }, 800);
-        }
+  $(window).scroll(function () {
+    $('.about').each(function (i) {
+      var bottom_of_object = Math.floor($(this).offset().top);
+      var bottom_of_window = $(window).scrollTop();
+      if (bottom_of_window >= bottom_of_object) {
+        $('.underlineWho').addClass('right');
+        setTimeout(function () {
+          $('.underlineWhat').addClass('right');
+        }, 800);
+        setTimeout(function () {
+          $('.front').addClass('right');
+        }, 1500);
+        setTimeout(function () {
+          $('.back').addClass('right');
+        }, 2200);
+        setTimeout(function () {
+          $('.etc').addClass('right');
+        }, 2900);
+        setTimeout(function () {
+          for (let j = 1; j < 16; j++) {
+            addHash(j);
+          }
+        }, 3300);
+      }
     });
-    $('.tools').each(function(i){
-      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-      var bottom_of_window = $(window).scrollTop() + $(window).height();
-         if( bottom_of_window > bottom_of_object ){
-           setTimeout(function(){
-            for (let j= 1; j < 11; j++){
-              addHash(j);
-            }
-           }, 1300);             
-        }       
-    }); 
   });
 
-  function addHash(j){
-    setTimeout(function(){
+  function addHash(j) {
+    setTimeout(function () {
       $("#stack" + j).addClass("width");
     }, 300 * j);
   }
 
-  function scroll_to(div){
+  function scroll_to(div) {
     $('html, body').animate({
       scrollTop: $(div).offset().top
-      }, 500);
-}
+    }, 500);
+  }
 
 });
